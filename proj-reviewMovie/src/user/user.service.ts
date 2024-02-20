9101c797e1a694a62c6402095e21dcd6740a3ce1
BielAlves1 <gabriel2002.alves1010@gmail.com>
2024-02-20 17:55:19 -0300

    CRUD ajustado

--- proj-reviewMovie/src/user/user.service.ts
+++ proj-reviewMovie/src/user/user.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -9,111 +14,115 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-      const user = await this.prisma.user.create({
-        data: {
-          ...createUserDto,
-          password: await bcrypt.hash(createUserDto.password, 10),
-        }
-      });
+    const user = await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: await bcrypt.hash(createUserDto.password, 10),
+      },
+    });
 
-      if (!user) {
-        throw new HttpException('Erro ao criar usuário', HttpStatus.BAD_REQUEST);
-      } else {
-        return {
-          ...user,
-          password: undefined,
-        };
-      }
+    if (!user) {
+      throw new HttpException('Erro ao criar usuário', HttpStatus.BAD_REQUEST);
+    } else {
+      return {
+        ...user,
+        password: undefined,
+      };
+    }
   }
 
   async findAll() {
-   
-      const users = await this.prisma.user.findMany();
+    const users = await this.prisma.user.findMany();
 
-      if (users.length === 0) {
-        throw new HttpException('Não foi possível listar usuários', HttpStatus.BAD_REQUEST);
-      } else {
-        return users.map(user => ({
-          ...user,
-          password: undefined,
-        }));
-      }
+    if (users.length === 0) {
+      throw new HttpException(
+        'Não foi possível listar usuários',
+        HttpStatus.BAD_REQUEST,
+      );
+    } else {
+      return users.map((user) => ({
+        ...user,
+        password: undefined,
+      }));
+    }
   }
 
   async findUserName(username: string) {
-      const user = await this.prisma.user.findUnique({
-        where: { username }
-      });
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+    });
 
-      if (!user) {
-        throw new NotFoundException('Erro: Usuário não encontrado.');
-      } else {
-        return {
-          ...user,
-          password: undefined,
-        };
-      }
+    if (!user) {
+      throw new NotFoundException('Erro: Usuário não encontrado.');
+    } else {
+      return {
+        ...user,
+        password: undefined,
+      };
+    }
   }
 
   async findUser(id: number) {
-      const user = await this.prisma.user.findUnique({
-        where: { id }
-      });
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
 
-      if (!user) {
-        throw new NotFoundException('Erro: Usuário não encontrado.');
-      } else {
-        return {
-          ...user,
-          password: undefined,
-        };
-      }
+    if (!user) {
+      throw new NotFoundException('Erro: Usuário não encontrado.');
+    } else {
+      return {
+        ...user,
+        password: undefined,
+      };
+    }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
-      const userData: any = { ...updateUserDto };
+    const userData: any = { ...updateUserDto };
 
-      if (userData.password) {
-        userData.password = await bcrypt.hash(updateUserDto.password, 10);
+    if (userData.password) {
+      userData.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
+
+    try {
+      const user = await this.prisma.user.update({
+        where: {
+          id,
+        },
+        data: userData,
+      });
+
+      return user;
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`Usuário com o ID '${id}' não encontrado.`);
+      } else {
+        throw new HttpException(
+          'Erro ao alterar dados do usuário.',
+          HttpStatus.BAD_REQUEST,
+        );
       }
-
-      try {
-        const user = await this.prisma.user.update({
-          where: {
-            id
-          },
-          data: userData
-        });
-
-        return user;
-
-      } 
-        catch (error) {
-        if (error.code === 'P2025') {
-          throw new NotFoundException(`Usuário com o ID '${id}' não encontrado.`);
-        } else {
-          throw new HttpException('Erro ao alterar dados do usuário.', HttpStatus.BAD_REQUEST);
-        }
-      }
-
+    }
   }
 
   async remove(id: number) {
+    try {
       const user = await this.prisma.user.delete({
-        where: { id }
+        where: { id },
       });
 
-      if (!user) {
-        throw new NotFoundException('Erro: Usuário não encontrado.');
+      return {
+        message: 'Usuário removido com sucesso.',
+      };
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`Usuário com o ID '${id}' não encontrado.`);
       } else {
-        return {
-          ...user,
-          password: undefined,
-        };
+      throw new HttpException('Erro ao remover usuário.', HttpStatus.BAD_REQUEST);
       }
-    
+    }
   }
 }
